25a35d20998c9091c52e74daafc510fe055cbaac
ahnaffauzan28 <mohammadahnaffauzan@gmail.com>
2023-11-26 22:40:33 +0700

    init

---
+++ app/Views/HelloWorld.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>HELLO WORLD</title>
+</head>
+<body
+    style="
+        display: flex;
+        justify-content: center;
+        flex-direction: column;
+        height: 100vh;
+        background-color: white;
+        "
+>
+  <h1  style="
+  text-align: center; 
+  fontsize: 10rem;
+  color: black">
+  Hello World.
+</h1>
+</body>
+</html>
